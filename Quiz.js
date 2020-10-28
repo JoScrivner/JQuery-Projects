c89d86b2c3e30de6b4f190f03cd13490981f60ec
AJ Hortobagyi <63471689+JoScrivner@users.noreply.github.com>
2020-10-28 19:32:39 +0100

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,37 @@
+function submitAnswers() {
+    var total = 5;
+    var score = 0;
+
+    //Get user input
+    var q1 = document.forms["quizForm"]["q1"].value;
+    var q2 = document.forms["quizForm"]["q2"].value;
+    var q3 = document.forms["quizForm"]["q3"].value;
+    var q4 = document.forms["quizForm"]["q4"].value;
+    var q5 = document.forms["quizForm"]["q5"].value;
+
+    //Validation
+
+    for (i = 1; i <= total; i++){
+        if(eval('q'+i) == null || eval('q'+i) == ''){
+            alert('You missed question ' + i);
+            return false;
+        }
+    }
+
+    //Set Correct Answers
+    var answers = ["a", "b", "c", "d", "a"];
+
+    //Check the answers
+    for (j = 1; j <= total; j++){
+        if(eval('q'+j) == answers[j-1]){
+            score++;
+        }
+    }
+
+    //Display Results
+    var results = document.getElementById('results');
+    results.innerHTML = "<h3>You scored <span>"+score+" out of "+total+"</span></h3>";
+    alert("You scored " +score+" out of "+total);
+
+    return false;
+}
